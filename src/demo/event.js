0ed02b11ada80f1659702be00ed47be290149d6b
luanxuechao <luanxuechaowd@gmail.com>
2019-10-16 14:11:48 +0800

    feat:添加文件

---
+++ src/demo/event.js
@@ -0,0 +1,24 @@
+// const EventEmitter = require('events');
+
+// let emitter = new EventEmitter();
+
+// emitter.on('myEvent', () => {
+//   console.log('hi 1');
+// });
+
+// emitter.on('myEvent', () => {
+//   console.log('hi 2');
+// });
+
+// emitter.emit('myEvent');
+
+const EventEmitter = require('events');
+
+let emitter = new EventEmitter();
+
+emitter.on('myEvent', () => {
+  console.log('hi');
+  emitter.emit('myEvent');
+});
+
+emitter.emit('myEvent');
